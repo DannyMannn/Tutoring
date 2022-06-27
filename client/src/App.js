57f3fbc3e27b3c96ab801348addb7955993e9da4
DannyMannn <danielsv1091@gmail.com>
2022-06-27 05:25:29 -0500

    Added Responsive Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+import './index.css';
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+
 function App() {
   const [ users, setUsers ] = useState([{}]);
 
@@ -15,20 +17,12 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <div className="main-wrapper">
+          <div className="content-wrapper">
+
+          </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/Navbar.js
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import { useState } from "react"
+
+const Navbar = () => {
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+    const toggle = () =>{
+        const toggleButton = document.getElementsByClassName('toggle-button')[0]
+        const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+        toggleButton.addEventListener('click', () => {
+            navbarLinks.classList.toggle('active')
+        })
+    }    
+
+    return (  
+        <nav>
+            <div className="main-wrapper">
+                <div className="nav-bar">
+                    <div className="main-logo">
+                        <a href="/"><img src="logo.png" id="main-logo" alt="" /></a>
+                    </div>
+                    <button 
+                    className="toggle-button"
+                    onClick={() => {
+                        setIsNavExpanded(!isNavExpanded);
+                        toggle();
+                      }}
+                      >
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </button>
+                    <div className="navbar-links">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/sessions">Sessions</a></li>
+                            <li><a href="teachers">Teachers</a></li>
+                            <li><a href="/profile">Profile</a></li> 
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </nav>
+    );
+}
+/*
+            <script>
+                const toggleButton = document.getElementsByClassName('toggle-button')[0]
+                const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+                toggleButton.addEventListener('click', () => {
+                    navbarLinks.classList.toggle('active')
+                })
+            </script>*/
+ 
+export default Navbar;
